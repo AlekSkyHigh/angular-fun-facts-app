4ff2a4461c26e1bef622cba1132606f847e00fe7
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-11-29 14:23:37 +0200

    footer component created and rendered

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
   ],
   imports: [
-    NavigationComponent,
     CommonModule,
-    HomeComponent
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   exports: [
     NavigationComponent,
+    FooterComponent,
     HomeComponent,
   ]
 })
