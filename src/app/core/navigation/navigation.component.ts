4a97f87ed4af7765d7a7696deece0cabe4a1802a
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-05 18:58:29 +0200

    logout initial and basic logic created

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from 'src/app/services/auth.service';
-import { SessionService } from 'src/app/services/session.service';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
 
-  constructor(
-    public authService: AuthService,
-    private sessionService: SessionService,
-    private router: Router) { }
+  constructor(public authService: AuthService) { }
+
+  logout() {
+    localStorage.removeItem('token')
+  }
 }
