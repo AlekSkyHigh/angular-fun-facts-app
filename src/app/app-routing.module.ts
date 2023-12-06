5bf409ff726241f9b34fe47ec2244d26e3984dcb
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-06 17:23:00 +0200

    dashboard routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/pages/home/home.component';
 import { RegisterComponent } from './core/pages/register/register.component';
 import { LoginComponent } from './core/pages/login/login.component';
+import { DashboardComponent } from './core/pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
