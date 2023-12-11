ad9fb099e54e8c0db7027e1412ab581cd24f5f22
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-11 20:03:03 +0200

    interceptor provided

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { AddHeaderInterceptor } from './interceptors/add-header.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,13 @@ import { CoreModule } from './core/core.module';
     HttpClientModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AddHeaderInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
