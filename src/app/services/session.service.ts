a654ccdd35ee0efaa4918f65f3d1531b88a2131e
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-12 15:37:19 +0200

    get-token function implemented

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class SessionService {
 
   constructor() { }
+
+  getToken(): any | null {
+    const token = localStorage.getItem('token');
+    if (token) {
+      return JSON.parse(token);
+    }
+
+    return null;
+  }
 }
