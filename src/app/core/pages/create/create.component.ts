b67d852f6a2dc09a706a817fd2b5b8fe797105b5
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-13 13:50:32 +0200

    create-page logic implemented

--- src/app/core/pages/create/create.component.ts
+++ src/app/core/pages/create/create.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { EntriesService } from 'src/app/services/entries.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  constructor(
+    private entryService: EntriesService,
+    private router: Router
+  ) { }
+
+  createEntry(form: NgForm) {
+
+    const { category, imageUrl, description, moreInfo } = form.value
+
+    if (form.invalid) { return }
+
+    const data = {
+      category,
+      imageUrl,
+      description,
+      moreInfo
+    }
+
+    this.entryService.createEntry(data).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        console.error('Error creating entry:', error);
+      }
+    });
+
+  }
 
 }
