a2b2dec0ffe9a24e90ed0a799c43f575522d18cd
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-13 14:14:01 +0200

    dashboard-page logic implemented

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EntriesService } from 'src/app/services/entries.service';
+import { Entry } from 'src/app/types/entry.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +10,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(private entriesService: EntriesService) { }
+
+  ngOnInit(): void {
+
+    this.entriesService.getAllEntries().subscribe({
+      next: (data) => {
+        this.entries = data;
+      },
+      error: (error) => {
+        console.error('Error fetching entries:', error);
+      },
+      complete: () => {
+        console.log('getAllEntries() completed');
+      }
+    });
+
+  }
 
 }
