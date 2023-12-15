f79d67742d061ee560ce0fc506c37c0fda0afe97
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-12-15 14:13:19 +0200

    details page routing implemented

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EntriesService } from 'src/app/services/entries.service';
 import { Entry } from 'src/app/types/entry.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
